8cda5a2968e3be32f41e029402fe0331fc579b23
Howard Yeend <howard.yeend@gmail.com>
2020-05-05 11:48:18 +0100

    Updates

---
+++ precache-manifest.9cdc93f28923a00cfe443f427340e304.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6db89e654a984d10f4a4b6508b4d9de3",
+    "url": "/prism/index.html"
+  },
+  {
+    "revision": "c9df10a4cc885547854a",
+    "url": "/prism/static/css/main.7e98e0ca.chunk.css"
+  },
+  {
+    "revision": "00b215284b7f4e772e32",
+    "url": "/prism/static/js/2.201b7822.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/prism/static/js/2.201b7822.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9df10a4cc885547854a",
+    "url": "/prism/static/js/main.8739df86.chunk.js"
+  },
+  {
+    "revision": "ee06f244988b98a974bb",
+    "url": "/prism/static/js/runtime-main.edab6668.js"
+  }
+]);
